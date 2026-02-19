6bf1ac8f2ba149e8830aaa23ae031f4d734b9908
Harold Prince E. dela Pena <harolddelapena.11@gmail.com>
2026-02-19 13:13:43 +0800

    fix: minor vercel fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import LandingPage from "./LandingPage";
 export default async function Home({
   searchParams,
 }: {
-  searchParams?:
-    | Record<string, string | string[] | undefined>
-    | Promise<Record<string, string | string[] | undefined>>;
+  searchParams?: Record<string, string | string[] | undefined>;
 }) {
-  const resolvedSearchParams = searchParams ? await searchParams : undefined;
-  return <LandingPage searchParams={resolvedSearchParams} />;
+  return <LandingPage searchParams={searchParams} />;
 }
